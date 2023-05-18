321f5d124fd3fd024b3100be24866a9ef15f1032
Pablo González L <pablgonz@yahoo.com>
2023-05-18 11:07:09 -0400

    Test JAVA 3

--- myjava.sh
+++ myjava.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 # Detección de java bajo MSYS2
 
+read -r -d '' usage <<-EOF
+Usage: `basename $0` [options]
+
+    -h, --help                Prints this help message
+    -y, --yes                 Say yes to all build script prompts
+    -d, --depsonly            Only install dependencies for MSYS2
+    -g, --ghactions           Disable install local dependencies
+    -p, --poppler <version>   Install poppler <version>
+    -P, --no-poppler          Skip build/install poppler
+    -F, --no-fontforge        Skip build/install fontforge
+
+Note: Nedd defined $JAVA_HOME or set java.exe under PATH
+
+EOF
+echo "$usage"
+
+
 # Si está definida en el entorno de variables Windows
 if [[ -n "$JAVA_HOME" ]]; then
     echo "Se encontró definida la variable de entorno Windows: JAVA_HOME."
